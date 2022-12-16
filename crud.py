1786089f537163074c49379c19122fb0d8b16b74
Darla Luangrath <darla.luangrath@gmail.com>
2022-12-16 15:50:01 -0800

    Implement direction and name

--- crud.py
+++ crud.py
@@ -3,43 +3,36 @@ from model import db, User, Recipe, Ingredient, connect_to_db
 
 def create_user(name, email, password):
     """Create and return a new user."""
-
     user = User(name=name, email=email, password=password)
-
     return user
 
 
 def get_user():
     """Return all users."""
-
     return User.query.all()
 
 
 def get_user_by_id(user_id):
     """Return a user by primary key."""
-
     return User.query.get(user_id)
 
 
 def get_user_by_email(email):
     """Return a user by email."""
-
     return User.query.filter(User.email == email).first()
 
 
 def get_user_by_name(name):
     """Return a user by name."""
-
     return User.query.filter(User.name == name).first()
 
 
 def create_recipe(title, summary, instructions):
     """Create and return a new recipe."""
-
     recipe = Recipe(
         recipe_name=title,
         description=summary,
-        direction=instructions
+        direction=instructions,
     )
 
     return recipe
@@ -47,18 +40,31 @@ def create_recipe(title, summary, instructions):
 
 def get_all_recipes():
     """Return all recipes."""
-
     return Recipe.query.all()
 
 
-def get_recipe_by_id(recipe_id):
-    """Return a recipe by primary key"""
+def get_recipe_by_name(recipe_name):
+    """Return all recipe by name."""
+    return Recipe.query.filter(Recipe.recipe_name).all()
 
+
+def get_recipe_by_id(recipe_id):
+    """Return a recipe by primary key."""
     return Recipe.query.get(recipe_id)
 
-def get_ingredient_by_id(ingredient_id):
-    """Return an ingredient by primary key"""
 
+def get_recipe_by_direction(direction):
+    """Return all recipes by instructions."""
+    return Recipe.query.get(direction)
+
+
+# def get_recipe_by_ingredient(rec_ingredient):
+#     """Return all ingredients from a recipe."""
+#     return Recipe.query.get(rec_ingredient)
+
+
+def get_ingredient_by_id(ingredient_id):
+    """Return an ingredient by primary key."""
     return Ingredient.query.get(ingredient_id)
 
 

527f5e873dbf1f945061cc8d6a2b9263f8fff9a4
Darla Luangrath <darla.luangrath@gmail.com>
2022-12-26 10:59:18 -0800

    Remove print statements

--- helper.py
+++ helper.py
@@ -4,12 +4,10 @@ import requests
 def get_recipes_by_ingredients(ingredients):
     """Search recipes by ingredients"""
     api_key = environ.get("SPOONACULAR_API_KEY", None)
-    # print(api_key)
     if not api_key:
         raise Exception("API key is not found. Did you forget to export it?")
 
     url = f"https://api.spoonacular.com/recipes/findByIngredients?apiKey={api_key}"
-    # print(url)
     payload = {'number': 3, 'ingredients': ingredients}
     response = requests.request("GET", url, params=payload)
     recipes = response.json()

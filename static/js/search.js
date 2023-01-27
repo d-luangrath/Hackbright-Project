3f11d0e7e22e31e9bb01549ce749fe4c3a8b8dec
Darla Luangrath <darla.luangrath@gmail.com>
2023-01-26 21:50:39 -0800

    clean up, adjust background colors to white, added icon to nav bar

--- static/js/search.js
+++ static/js/search.js
@@ -23,7 +23,7 @@ let form = document.querySelector('#search').addEventListener("submit", (evt) =>
           <div class="card h-80 text-center box-shadow mb-3 mx-auto my-5" style="width: 25rem;">
             <img src="${recipe.image_url}" class="card-img-top">
             <div class="card-body">
-              <a class="card-title" href="/recipe/${recipe.id}">${recipe.title}</a>
+              <a class="card-title"  style="text-decoration: none;" href="/recipe/${recipe.id}">${recipe.title}</a>
               <hr>
               <button class="btn btn-outline-danger my-2 my-sm-0" id="fav-btn" type="button" data-recipe-id="${recipe.id}">Favorite</button>
             </div>
